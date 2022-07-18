bfd425208cf0958c8f74c3020e398786822de23a
Igor <filinigor@yandex.ru>
2022-07-18 23:06:48 +0300

    1 commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <span><NavLink to={'/'}>main</NavLink></span>
+            <span> </span>
+            <span><NavLink to={'/login'}>login</NavLink></span>
+            <span> </span>
+            <span><NavLink to={'/profile'}>profile</NavLink></span>
+            <span> </span>
+            <span><NavLink to={'/profile/settings'}>settings</NavLink></span>
+            <Routes>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile/*'} element={
+                    <div>
+                        profile
+                        <Routes>
+                            <Route path={'/settings'} element={<div>settings</div>}/>
+                        </Routes>
+                    </div>}
+                />
+
+            </Routes>
+
+
+        </div>
+    );
 }
 
 export default App;
