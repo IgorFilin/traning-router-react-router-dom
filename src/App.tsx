bd4eeec03b2272ea4d5aeb9f3d685575a01d69c6
Igor <filinigor@yandex.ru>
2022-07-20 21:17:42 +0300

    add useSearchParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet, Route, Routes, useNavigate, useParams} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes, useNavigate, useParams, useSearchParams} from "react-router-dom";
 
 function App() {
-
 
 
     return (
@@ -28,28 +27,28 @@ function App() {
             {/*        <Route path={'/profile/settings'} element={<div>settings</div>}/>*/}
             {/*    </Route>*/}
             {/*</Routes>*/}
-            <NavLink to='/' style={({isActive})=>{ return {color:isActive ?"lime": 'black'}}}>MAIN --</NavLink>
-            <NavLink to='/users'>USERS --</NavLink>
-            <NavLink to='/messages'>MESSAGES --</NavLink>
-            <NavLink to='/profile'>PROFILE --</NavLink>
-            <NavLink to='/profile/myPosts'>MYPOST --</NavLink>
-            <NavLink to='/users/1/Igor'>USER ID 1 </NavLink>
+            <NavLink to='/' style={({isActive}) => {
+                return {color: isActive ? "lime" : 'black'}
+            }}>MAIN --</NavLink>
+            <NavLink className={(s) => s.isActive ? 'activ' : ''} to='/users'>USERS --</NavLink>
+            <NavLink className={(s) => s.isActive ? 'activ' : ''} to='/messages'>MESSAGES --</NavLink>
+            <NavLink className={(s) => s.isActive ? 'activ' : ''} to='/profile'>PROFILE --</NavLink>
+            <NavLink className={(s) => s.isActive ? 'activ' : ''} to='/profile/myPosts'>MYPOST --</NavLink>
+            <NavLink className={(s) => s.isActive ? 'activ' : ''} to='/users/1/Igor'>USER ID 1 </NavLink>
 
 
             <Routes>
-                <Route path='/*' element={<div>Error 404</div>} />
+                <Route path='/*' element={<div>Error 404</div>}/>
                 <Route path='/' element={<div>MAIN</div>}/>
                 <Route path='/users/:id/:name' element={<Users/>}/>
-                <Route path='/messages' element={<div>MY MESSAGES<Outlet/></div>}>
-                <Route index element={<div>CHANGE MESSAGES</div>} />
+                <Route path='/messages' element={<div><Outlet/><Messages/></div>}>
+                    <Route index element={<div>CHANGE MESSAGES</div>}/>
                     <Route path=':messages' element={<div>NEW MESSAGES</div>}/>
                 </Route>
                 <Route path='/profile' element={<div>MY PROFILE<Outlet/></div>}>
                     <Route path=':nameID' element={<div>ID</div>}/>
                     <Route path='/profile/myPosts' element={<div>MY POST</div>}/>
                 </Route>
-
-
 
 
             </Routes>
@@ -59,15 +58,35 @@ function App() {
     );
 }
 
-export const Users= () => {
-    const param = useParams<'id'|'name'>()
+export const Users = () => {
+    const param = useParams<'id' | 'name'>()
     console.log(param)
-   const navigate =  useNavigate()
+    const navigate = useNavigate()
 
-    return(<div>USERS<button onClick={()=> {navigate('/messages')}}>messages</button></div>)
+    return (<div>USERS
+        <button onClick={() => {
+            navigate('/messages')
+        }}>messages
+        </button>
+    </div>)
 
 }
 
+export const Messages = () => {
+   const [perem,setPerem] = useSearchParams()
+    console.log(perem.get('message'))
+    console.log(Object.fromEntries(perem))
+
+    return (<div>
+        <ul>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </ul>
+    </div>)
+
+}
 
 
 export default App;
